b5388ea2bfd66b78487e033e38126b0fe401d920
Alex <alexgillard04@gmail.com>
2022-08-09 16:35:42 +0200

    Initial commit

---
+++ DaysLoop/Counter.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DaysLoop
+{
+    /// <summary>
+    /// Interaction logic for Counter.xaml
+    /// </summary>
+    public partial class Counter : UserControl
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public Counter()
+        {
+            InitializeComponent();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+            updateTimer();
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            updateTimer();
+        }
+        void updateTimer()
+        {
+            DateTime now = DateTime.Now;
+            DateTime date1 = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+            DateTime date2 = new DateTime(now.Year, now.Month, now.Day, 17, 0, 0);
+            TimeSpan value = date2.Subtract(date1);
+            lblTimeLeft.Content = value;
+        }
+    }
+}
---
+++ DaysLoop/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace DaysLoop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            base.Close();
+        }
+
+        private void btnClose_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnClose.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void btnClose_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnClose.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            base.WindowState = WindowState.Minimized;
+        }
+    }
+}
